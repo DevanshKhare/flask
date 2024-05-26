5cf79c9797a51d86461491b0263fadec9e6960a6
Devansh Khare <devanshkhare0@gmail.com>
2024-05-26 12:12:45 +0530

    created dynamic route

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask
-
+from markupsafe import escape
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "<p>Hello, World</p>"
+
+@app.route("/<name>")
+def dynamic(name):
+    return f"Hello, {escape(name)}"
